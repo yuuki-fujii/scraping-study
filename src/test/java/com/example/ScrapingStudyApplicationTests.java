5818c89b998d320132654d7c85ef12fc4ab069d6
fujiiyuuki <yuki.fujii@rakus-partners.co.jp>
2020-11-11 11:11:06 +0900

    first commit

---
+++ src/test/java/com/example/ScrapingStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapingStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
